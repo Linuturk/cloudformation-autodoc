99c826db24dd3d2ba18bcb3e7f878a40ff0cb67e
Justin Phelps <linuturk@onitato.com>
2016-03-03 15:28:22 -0600

    setup.py for the module

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='cfautodoc',
+      version='0.1',
+      description='Tool to automatically document CloudFormation templates in Markdown.',
+      long_description=readme(),
+      url='http://github.com/linuturk/cloudformation-autodoc',
+      author='Justin Phelps',
+      author_email='linuturk@onitato.com',
+      license='MIT',
+      packages=['cfautodoc'],
+      scripts=['bin/cfautodoc'],
+      install_requires=[
+          'argparse',
+      ],
+      zip_safe=False)
