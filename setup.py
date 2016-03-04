387463f4daaa90761493c3b16369f3aa04792634
Justin Phelps <linuturk@onitato.com>
2016-03-04 10:40:28 -0600

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
         return f.read()
 
 setup(name='cfautodoc',
-      version='0.1.2',
+      version='0.1.3',
       description='Tool to document CloudFormation templates in Markdown.',
       long_description=readme(),
       url='http://github.com/linuturk/cloudformation-autodoc',
